563660a6419f3f7d340018221c6ecdedb6bed29d
Dipesh Rijal <dipesh.rijal@gmail.com>
2016-01-06 11:33:01 +0545

    laravel-permission

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
             <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
                 <!-- Styles -->
                 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-                {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+                <link href="/css/bootstrap.min.css" rel="stylesheet">
                 <style>
                         body {
                             font-family: 'Lato';
@@ -69,8 +69,8 @@
                 </div>
 
                 <!-- JavaScripts -->
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+                <script src="/js/jquery.min.js"></script>
+                <script src="/js/bootstrap.min.js"></script>
                 {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
             </body>
         </html>
